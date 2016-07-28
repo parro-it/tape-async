506924426f31c1d79220d4c14876794ee88f6910
Stan Bondi <stan@fixate.it>
2016-07-28 10:42:55 +0200

    Removed surpurflous tests
    For this purpose there is no difference between arrow and normal
    function declarations.

--- test.js
+++ test.js
@@ -96,23 +96,12 @@ test('support sync function with plan', function(t) {
   t.equal(result, 42);
 });
 
-test('support arrows with plan', t => {
-  t.plan(1);
-  const result = 42;
-  t.equal(result, 42);
-});
-
 test('support generators', function *(t) {
   const result = yield Promise.resolve(42);
   t.equal(result, 42);
 });
 
 test('support sync function', function(t) {
-  const result = 42;
-  t.equal(result, 42);
-});
-
-test('support arrows', t => {
   const result = 42;
   t.equal(result, 42);
 });
